960bc1389723fc863569829af3361dc6524d289e
KRIPOTA <dyupin.lesha@yandex.ru>
2023-06-16 15:59:28 +0700

    commit #2

--- crmamotestbackend/index.js
+++ crmamotestbackend/index.js
@@ -5,8 +5,18 @@ var cors = require('cors');
 const port = 3002;
 const app = express();
 
-
 app.use(cors())
+var access_token;
+axios.get('https://test.gnzs.ru/oauth/get-token.php',
+    {
+        headers: {
+            'X-Client-Id': '31122838',
+        }
+    })
+    .then((response) => {
+        access_token = response.data.access_token
+    })
+
 
 app.get(`/leads`, (request, res) => {
     axios.post('https://dyupinlesha.amocrm.ru/api/v4/leads', [
@@ -16,7 +26,7 @@ app.get(`/leads`, (request, res) => {
     ],
         {
             headers: {
-                Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijc2MTMwYjFkOGI3OWU3ZGY3MDdhM2MzYzE0NjcwNDc2NzZkYjc4YTFhZmRmNjg4N2E2YzVlZDVhYWQzYWI3Y2Q4ODNhMWI0NDE1YTJkODI2In0.eyJhdWQiOiJhZmY2ZmYxMS01Mzc4LTRmYmUtODQ1Yi0wMDBmZWRlNDZjYWIiLCJqdGkiOiI3NjEzMGIxZDhiNzllN2RmNzA3YTNjM2MxNDY3MDQ3Njc2ZGI3OGExYWZkZjY4ODdhNmM1ZWQ1YWFkM2FiN2NkODgzYTFiNDQxNWEyZDgyNiIsImlhdCI6MTY4NjgzNDQzNiwibmJmIjoxNjg2ODM0NDM2LCJleHAiOjE2ODY5MjA4MzYsInN1YiI6Ijk3MzEyNDYiLCJncmFudF90eXBlIjoiIiwiYWNjb3VudF9pZCI6MzExMjI4MzgsImJhc2VfZG9tYWluIjoiYW1vY3JtLnJ1IiwidmVyc2lvbiI6InYyIiwic2NvcGVzIjpbImNybSJdfQ.Cd5JKNAVPk0K_jSRMhnn60MKDCv5SHH3QAOJ7ns3-zPEiPVp0_zGrSxhzP1-S5j6Q9kwOACgz3hnrJNQNKNV7D_JanWstcjTYhWCrJxAOq5Mx9H-wfL5YsMsfjWfw-ZqsJ9ZtYXM54X4Lw2YcCyR0THxpEpiZ4tE-lTmIyr0WpGKlJqgVzxNZ7-ETd4sAyLIb7A8psz_YFPHnBCFsDmFy-O3ALQ1NT53OmOsVdWgACJQfQ5PpwADHQqXYeW_9qT07oVaSMCLdGmeedTzy2GteUTxJLD8Bsb0vvy7q8NjEgUG3p1lZn03QpBMxQ6Zrp4FwkNX30JfneDAGSdHJoq8GA',
+                Authorization: 'Bearer ' + access_token,
             }
         })
         .then((response) => {
@@ -32,7 +42,7 @@ app.get(`/contacts`, (request, res) => {
     ],
         {
             headers: {
-                Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijc2MTMwYjFkOGI3OWU3ZGY3MDdhM2MzYzE0NjcwNDc2NzZkYjc4YTFhZmRmNjg4N2E2YzVlZDVhYWQzYWI3Y2Q4ODNhMWI0NDE1YTJkODI2In0.eyJhdWQiOiJhZmY2ZmYxMS01Mzc4LTRmYmUtODQ1Yi0wMDBmZWRlNDZjYWIiLCJqdGkiOiI3NjEzMGIxZDhiNzllN2RmNzA3YTNjM2MxNDY3MDQ3Njc2ZGI3OGExYWZkZjY4ODdhNmM1ZWQ1YWFkM2FiN2NkODgzYTFiNDQxNWEyZDgyNiIsImlhdCI6MTY4NjgzNDQzNiwibmJmIjoxNjg2ODM0NDM2LCJleHAiOjE2ODY5MjA4MzYsInN1YiI6Ijk3MzEyNDYiLCJncmFudF90eXBlIjoiIiwiYWNjb3VudF9pZCI6MzExMjI4MzgsImJhc2VfZG9tYWluIjoiYW1vY3JtLnJ1IiwidmVyc2lvbiI6InYyIiwic2NvcGVzIjpbImNybSJdfQ.Cd5JKNAVPk0K_jSRMhnn60MKDCv5SHH3QAOJ7ns3-zPEiPVp0_zGrSxhzP1-S5j6Q9kwOACgz3hnrJNQNKNV7D_JanWstcjTYhWCrJxAOq5Mx9H-wfL5YsMsfjWfw-ZqsJ9ZtYXM54X4Lw2YcCyR0THxpEpiZ4tE-lTmIyr0WpGKlJqgVzxNZ7-ETd4sAyLIb7A8psz_YFPHnBCFsDmFy-O3ALQ1NT53OmOsVdWgACJQfQ5PpwADHQqXYeW_9qT07oVaSMCLdGmeedTzy2GteUTxJLD8Bsb0vvy7q8NjEgUG3p1lZn03QpBMxQ6Zrp4FwkNX30JfneDAGSdHJoq8GA',
+                Authorization: 'Bearer ' + access_token,
             }
         })
         .then((response) => {
@@ -48,7 +58,7 @@ app.get(`/companies`, (request, res) => {
     ],
         {
             headers: {
-                Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijc2MTMwYjFkOGI3OWU3ZGY3MDdhM2MzYzE0NjcwNDc2NzZkYjc4YTFhZmRmNjg4N2E2YzVlZDVhYWQzYWI3Y2Q4ODNhMWI0NDE1YTJkODI2In0.eyJhdWQiOiJhZmY2ZmYxMS01Mzc4LTRmYmUtODQ1Yi0wMDBmZWRlNDZjYWIiLCJqdGkiOiI3NjEzMGIxZDhiNzllN2RmNzA3YTNjM2MxNDY3MDQ3Njc2ZGI3OGExYWZkZjY4ODdhNmM1ZWQ1YWFkM2FiN2NkODgzYTFiNDQxNWEyZDgyNiIsImlhdCI6MTY4NjgzNDQzNiwibmJmIjoxNjg2ODM0NDM2LCJleHAiOjE2ODY5MjA4MzYsInN1YiI6Ijk3MzEyNDYiLCJncmFudF90eXBlIjoiIiwiYWNjb3VudF9pZCI6MzExMjI4MzgsImJhc2VfZG9tYWluIjoiYW1vY3JtLnJ1IiwidmVyc2lvbiI6InYyIiwic2NvcGVzIjpbImNybSJdfQ.Cd5JKNAVPk0K_jSRMhnn60MKDCv5SHH3QAOJ7ns3-zPEiPVp0_zGrSxhzP1-S5j6Q9kwOACgz3hnrJNQNKNV7D_JanWstcjTYhWCrJxAOq5Mx9H-wfL5YsMsfjWfw-ZqsJ9ZtYXM54X4Lw2YcCyR0THxpEpiZ4tE-lTmIyr0WpGKlJqgVzxNZ7-ETd4sAyLIb7A8psz_YFPHnBCFsDmFy-O3ALQ1NT53OmOsVdWgACJQfQ5PpwADHQqXYeW_9qT07oVaSMCLdGmeedTzy2GteUTxJLD8Bsb0vvy7q8NjEgUG3p1lZn03QpBMxQ6Zrp4FwkNX30JfneDAGSdHJoq8GA',
+                Authorization: 'Bearer ' + access_token,
             }
         })
         .then((response) => {
